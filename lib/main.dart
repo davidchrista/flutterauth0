721ee1934bcd41931796efa479f72dec4a8fc32d
David Christa <davidchrista@gmail.com>
2024-03-09 23:28:36 +0100

    set up auth0 integration

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@
 import 'package:auth0_flutter/auth0_flutter.dart';
 import 'package:flutter/material.dart';
 
+const appScheme = 'flutterauth0';
+
 /// -----------------------------------
 ///           Profile Widget
 /// -----------------------------------
@@ -94,6 +96,48 @@ class _MyAppState extends State<MyApp> {
   bool isBusy = false;
   late String errorMessage;
 
+  Credentials? _credentials;
+  late Auth0 auth0;
+
+  void initState() {
+    super.initState();
+
+    auth0 = Auth0('dev-gzm0pgbh.us.auth0.com', 'Lc5xWjq3AmxXL4Nyhp9QWspQF2psuEUm');
+    errorMessage = '';
+  }
+
+  Future<void> loginAction() async {
+    setState(() {
+      isBusy = true;
+      errorMessage = '';
+    });
+
+    try {
+      final Credentials credentials =
+          await auth0.webAuthentication(scheme: appScheme).login();
+
+      setState(() {
+        isBusy = false;
+        _credentials = credentials;
+      });
+    } on Exception catch (e, s) {
+      debugPrint('login error: $e - stack: $s');
+
+      setState(() {
+        isBusy = false;
+        errorMessage = e.toString();
+      });
+    }
+  }
+
+  Future<void> logoutAction() async {
+    await auth0.webAuthentication(scheme: appScheme).logout();
+
+    setState(() {
+      _credentials = null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -103,7 +147,11 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Auth0 Demo'),
         ),
         body: Center(
-          child: const Text('Implement User Authentication'),
+          child: isBusy
+              ? const CircularProgressIndicator()
+              : _credentials != null
+                  ? Profile(logoutAction, _credentials?.user)
+                  : Login(loginAction, errorMessage),
         ),
       ),
     );
